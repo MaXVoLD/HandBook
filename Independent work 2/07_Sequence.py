bee9ecc4abfd7cfd5be9b1c7678bfa92b3e45243
MaXVoLD <165142130+MaXVoLD@users.noreply.github.com>
2024-08-07 20:32:19 +0300

    Fix print

--- Independent work 2/07_Sequence.py
+++ Independent work 2/07_Sequence.py
@@ -6,6 +6,8 @@ def sequence():
         list_value = sorted([value_1, value_2, value_3])
         if int(list_value[1]) - list_value[0] == list_value[2] - list_value[1]:
             print('Арифмитическая прогрессия есть! \nПрограмма завершена')
+        else:
+            print('Арифмитической прогрессии нет! \nПрограмма завершена')
     except ValueError:
         print('\nВведёно некорректное значение! \nПовторите попытку!\n')
         sequence()
