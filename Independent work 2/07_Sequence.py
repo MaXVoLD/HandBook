46bf0f6a3c1e05f88f3ada366d9cd46f0d3a7534
MaXVoLD <165142130+MaXVoLD@users.noreply.github.com>
2024-08-07 20:18:49 +0300

    Add file

---
+++ Independent work 2/07_Sequence.py
@@ -0,0 +1,14 @@
+def sequence():
+    try:
+        value_1 = int(input('Введите первое натуральное число: '))
+        value_2 = int(input('Введите второе натуральное число: '))
+        value_3 = int(input('Введите третье натуральное число: '))
+        list_value = sorted([value_1, value_2, value_3])
+        if int(list_value[1]) - list_value[0] == list_value[2] - list_value[1]:
+            print('Арифмитическая прогрессия есть! \nПрограмма завершена')
+    except ValueError:
+        print('\nВведёно некорректное значение! \nПовторите попытку!\n')
+        sequence()
+
+
+sequence()
